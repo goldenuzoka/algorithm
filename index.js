3b640b38c73621445ceb9025bf3138ce0d049380
goldenuzoka <uzokai69@gmail.com>
2023-09-08 10:01:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+sentence_length = 0
+word_count = 0
+vowel_count = 0
+
+
+sentence = input("Enter a sentence ending with a point: ")
+
+
+
+
+    sentence_length += 1
+
+
+        word_count += 1
+
+
+        vowel_count += 1
+
+
+word_count += 1
+
+
+print("Length of the sentence:", sentence_length)
+print("Number of words in the sentence:", word_count)
+print("Number of vowels in the sentence:", vowel_count)
